26990614faa857acf45ad78eb89406a3419463b1
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2023-02-09 14:49:49 +0300

    Dump info added

--- WorldCupDB/worldcup.sql
+++ WorldCupDB/worldcup.sql
@@ -1,1 +1,6 @@
-TODO
+--
+-- Dump was maded by entering in a bash terminal:       pg_dump -cC --inserts -U freecodecamp worldcup > worldcup.sql
+-- Database can be rebuilded from dump:                 psql -U postgres < worldcup.sql
+--
+
+-- TODO
