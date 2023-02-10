a6ec6df431b94f75638bfe856df6946c6cd4d058
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2023-02-10 11:16:57 +0300

    Update insert_data.sh

--- WorldCupDB/insert_data.sh
+++ WorldCupDB/insert_data.sh
@@ -1,1 +1,46 @@
-# TODO
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+echo $($PSQL "TRUNCATE games, teams")  # clear tables before new script run
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR != "year" ]]  # check if string is not a table header 
+  then
+
+    # get winner team_id
+    WINNER_TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    # if not found
+    if [[ -z $WINNER_TEAM_ID ]]  # if WINNER_TEAM_ID string is empty
+    then
+      # insert winner team
+      INSERT_TEAM_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      if [[ $INSERT_TEAM_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted into teams: $WINNER
+      fi
+    fi
+
+    # get opponent team_id
+    OPPONENT_TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    # if not found
+    if [[ -z $OPPONENT_TEAM_ID ]]  # if OPPONENT_TEAM_ID string is empty
+    then
+      # insert opponent team
+      INSERT_TEAM_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      if [[ $INSERT_TEAM_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted into teams: $OPPONENT
+      fi
+    fi
+
+  fi 
+done
