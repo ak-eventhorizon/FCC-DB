23116fef7cde5fc56551d42d600e1621e7d90bb5
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2023-02-28 13:54:19 +0300

    Update periodic_table.sql

---
+++ PeriodTableDB/periodic_table.sql
@@ -0,0 +1,5 @@
+--
+-- Dump was maded by entering in a bash terminal:       pg_dump -cC --inserts -U freecodecamp periodic_table > periodic_table.sql
+-- Database can be rebuilded from dump:                 psql -U postgres < periodic_table.sql
+--
+
