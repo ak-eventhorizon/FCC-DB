aa60c446609bd3e1a6174a69875b2e35fb7e75fe
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2023-03-03 15:41:42 +0300

    feat: mvp done

--- NumGuessing/number_guess.sh
+++ NumGuessing/number_guess.sh
@@ -2,6 +2,31 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
+
+# ---------- Authorize part ----------
+echo -e "Enter your username:"
+read USER_NAME
+CURRENT_USER_QUERY=$($PSQL "SELECT * FROM number_guess WHERE name = '$USER_NAME'")
+
+# if no such user in database
+if [[ -z $CURRENT_USER_QUERY ]]
+then
+  echo -e "Welcome, $USER_NAME! It looks like this is your first time here."
+  
+  NAME=$USER_NAME
+  GAMES_PLAYED=0
+  BEST_GAME=0
+
+  CREATE_NEW_USER=$($PSQL "INSERT INTO number_guess(name, games_played, best_game) VALUES('$NAME', $GAMES_PLAYED, $BEST_GAME)")
+else
+  echo -e "$CURRENT_USER_QUERY" | while IFS="|" read USER_ID NAME GAMES_PLAYED BEST_GAME  # 3|Alex|13|8
+  do
+    echo -e "Welcome back, $NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  done
+fi
+
+
+# ---------- Game part ----------
 CURRENT_RANDOM=$((1 + $RANDOM % 1000)) # random number between 1 and 1000 (inclusive)
 GUESS_ATTEMPTS=1
 IS_NUMBER_GUESSED=false
@@ -31,6 +56,16 @@ do
     then
       IS_NUMBER_GUESSED=true
       echo -e "You guessed it in $GUESS_ATTEMPTS tries. The secret number was $CURRENT_RANDOM. Nice job!"
+      
+      CURRENT_USER_QUERY=$($PSQL "SELECT * FROM number_guess WHERE name = '$USER_NAME'")
+      echo -e "$CURRENT_USER_QUERY" | while IFS="|" read USER_ID NAME GAMES_PLAYED BEST_GAME
+      do
+        GAMES_INCREMENT=$($PSQL "UPDATE number_guess SET games_played = games_played+1 WHERE name = '$NAME'")
+        if [[ $BEST_GAME -eq 0 || $GUESS_ATTEMPTS -lt $BEST_GAME ]]
+        then
+          BEST_GAME_UPDATE=$($PSQL "UPDATE number_guess SET best_game = $GUESS_ATTEMPTS WHERE name = '$NAME'")
+        fi
+      done
     fi
 
   else
